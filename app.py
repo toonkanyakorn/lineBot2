3e962dddb02b4d5bb37455ce4a27915db57778ad
toonkanyakorn <51436418+toonkanyakorn@users.noreply.github.com>
2019-07-11 14:20:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,14 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    if (userText == 'กินข้าวหรือยัง') :
+        sendText(user,'กินเเล้ว')
+    elif (userText == 'ไปเที่ยวกันไหม') :
+        sendText(user,'ไปคนับ')
+    else :
+        sendText(user,'พิมรายยยมาอ่ะ')
+
     return '',200
 
 def sendText(user, text):
